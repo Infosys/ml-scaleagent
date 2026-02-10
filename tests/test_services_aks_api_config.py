d4c04263b42dea959aa7e6441f076025838bc5bb
Sourabh Panwar <sourabh.panwar@ad.infosys.com>
2026-02-10 17:50:30 +0530

    CLI methods implementation

--- tests/test_services_aks_api_config.py
+++ tests/test_services_aks_api_config.py
@@ -1,3 +1,19 @@
+# /******************************************************************
+# © 2025-2026 Infosys Limited, Bangalore, India. All Rights Reserved.
+# Infosys believes the information in this document is accurate as of its publication date;
+# such information is subject to change without notice. Infosys acknowledges the proprietary
+# rights of other companies to the trademarks, product names and such other intellectual property
+# rights mentioned in this document. Except as expressly permitted, neither this documentation nor
+# any part of it may be reproduced, stored in a retrieval system, or transmitted in any form or by
+# any means, electronic, mechanical, printing, photocopying, recording or otherwise, without the prior
+# permission of Infosys Limited and/or any named intellectual property rights holders under this document.
+# *******************************************************************/
+
+# PROPRIETARY NOTICE
+# This software is confidential and proprietary information of Infosys Limited.
+# You shall not disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into with Infosys Limited.
+
 import unittest
 from app.services import aks_api_config
 
