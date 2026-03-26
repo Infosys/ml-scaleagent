d67c65e2479193bebb5d22e0ecbd17dc97d68e79
vivekvj2026 <vivek.vj@infosys.com>
2026-03-26 13:22:00 -0600

    Update functionone.py

--- workflow/src/workflow_1_code/functionone.py
+++ workflow/src/workflow_1_code/functionone.py
@@ -1,7 +1,7 @@
 from .functiontwo import functiontwo
 
 def functionone(events):
-    # functionone, functiontwo and functionthree are examples/teamplates.
-    # In developement environment, these functions will contain the actual inference logic.
-    # The function names inside the workflow_1_code can be changed to represnt the real world model use case.
+    # functionone, functiontwo and functionthree are examples/templates.
+    # In actual developement environment, these functions will contain the actual inference logic.
+    # The function names and definition can be changed to represnt the real world use case.
     return functiontwo(events)
